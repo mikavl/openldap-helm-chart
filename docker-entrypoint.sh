e6d45e65d0f72b77cc4148a228fa3be2115fbf3d
mikavl <117277985+mikavl@users.noreply.github.com>
2023-10-06 16:06:46 +0300

    Add manager configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,14 +15,18 @@ fi
 
 mkdir -p /var/lib/ldap/accesslog
 
-credentials="$(cat "$secret_dir/credentials")"
+manager="$(cat "$secret_dir/manager")"
+manager_credentials="$(cat "$secret_dir/manager_credentials")"
 replicator="$(cat "$secret_dir/replicator")"
+replicator_credentials="$(cat "$secret_dir/replicator_credentials")"
 suffix="$(cat "$secret_dir/suffix")"
 
 sed --regexp-extended \
-  --expression="s/@CREDENTIALS@/$credentials/" \
   --expression="s/@GID@/$(id -g)/" \
+  --expression="s/@MANAGER@/$manager/" \
+  --expression="s/@MANAGER CREDENTIALS@/$manager_credentials/" \
   --expression="s/@REPLICATOR@/$replicator/" \
+  --expression="s/@REPLICATOR CREDENTIALS@/$replicator_credentials/" \
   --expression="s/@SUFFIX@/$suffix/" \
   --expression="s/@UID@/$(id -u)/" \
   "/etc/ldap/init/slapd.init.ldif" "/etc/ldap/init/slapd.$role.init.ldif" \
