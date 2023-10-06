08cd239cf291a6fd42305fd2dd15da6b79745d1a
mikavl <117277985+mikavl@users.noreply.github.com>
2023-10-06 17:18:36 +0300

    Make config and data share a volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,32 +3,34 @@
 set -e
 set -u
 
-secret_dir="/etc/ldap/secret"
+if [[ ! -f "/etc/ldap/slapd.d/cn=config.ldif" ]]; then
+  secret_dir="/etc/ldap/secret"
 
-pod="$(hostname)"
+  pod="$(hostname)"
 
-if [[ "${pod#*-}" == "0" ]]; then
-    role="provider"
-else
-    role="consumer"
+  if [[ "${pod#*-}" == "0" ]]; then
+      role="provider"
+  else
+      role="consumer"
+  fi
+
+  mkdir -p /var/lib/ldap/accesslog
+
+  manager="$(cat "$secret_dir/manager")"
+  replicator="$(cat "$secret_dir/replicator")"
+  replicator_credentials="$(cat "$secret_dir/replicator_credentials")"
+  suffix="$(cat "$secret_dir/suffix")"
+
+  sed --regexp-extended \
+    --expression="s/@GID@/$(id -g)/" \
+    --expression="s/@MANAGER@/$manager/" \
+    --expression="s/@REPLICATOR@/$replicator/" \
+    --expression="s/@REPLICATOR CREDENTIALS@/$replicator_credentials/" \
+    --expression="s/@SUFFIX@/$suffix/" \
+    --expression="s/@UID@/$(id -u)/" \
+    "/etc/ldap/init/slapd.init.ldif" "/etc/ldap/init/slapd.$role.init.ldif" \
+    | slapadd -d -1 -F /etc/ldap/slapd.d -n 0
 fi
-
-mkdir -p /var/lib/ldap/accesslog
-
-manager="$(cat "$secret_dir/manager")"
-replicator="$(cat "$secret_dir/replicator")"
-replicator_credentials="$(cat "$secret_dir/replicator_credentials")"
-suffix="$(cat "$secret_dir/suffix")"
-
-sed --regexp-extended \
-  --expression="s/@GID@/$(id -g)/" \
-  --expression="s/@MANAGER@/$manager/" \
-  --expression="s/@REPLICATOR@/$replicator/" \
-  --expression="s/@REPLICATOR CREDENTIALS@/$replicator_credentials/" \
-  --expression="s/@SUFFIX@/$suffix/" \
-  --expression="s/@UID@/$(id -u)/" \
-  "/etc/ldap/init/slapd.init.ldif" "/etc/ldap/init/slapd.$role.init.ldif" \
-  | slapadd -d -1 -F /etc/ldap/slapd.d -n 0
 
 ulimit -n 1024
 exec "$@"
